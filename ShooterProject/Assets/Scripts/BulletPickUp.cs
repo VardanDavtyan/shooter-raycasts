29653eb1aca6629afb445b5a4b6df15129d2ee9a
Vardan <vardan.davtyan.y@tumo.org>
2021-05-08 17:36:23 +0400

    Project Source

---
+++ ShooterProject/Assets/Scripts/BulletPickUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BulletPickUp : MonoBehaviour {
+
+    public PlayerScript playerScript;
+    [SerializeField] private int bulletPickUpCount = 25;
+    [SerializeField] private float rotateSpeed = 200f;
+
+    private void Start() {
+
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.tag.Equals(playerScript.tag)) {
+            playerScript.AddBullets(bulletPickUpCount);
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Update () {
+
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+
+    }
+
+}
---
+++ ShooterProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Animator anim;
+    public Transform target;
+    public Transform camTransform;
+    public GameObject explosion;
+    public ParticleSystem Flare;
+
+    public Text currentAmmoCountText, slotsText;
+
+    //shooting effects
+    [SerializeField] private float range = 100f;
+    [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float nextTimeToFire = 0f;
+    [SerializeField] private float reloadTime = 1.4f;
+    public float weaponDamage = 20f;
+
+    //ammo
+    public int availableSlots;
+    public float totalAmmo = 70f;
+    public float maxCountOfCurrentAmmo = 25f;
+    public float currentAmmo = 25f;
+
+    private void Start() {
+
+        anim = GetComponent<Animator>();
+        totalAmmo -= currentAmmo;
+        availableSlots = (int)Mathf.Ceil(totalAmmo / maxCountOfCurrentAmmo);
+        SetUI();
+
+    }
+
+    private void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R) && totalAmmo > 0) {
+            StartCoroutine(Reload());
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+		
+	}
+
+    public IEnumerator Reload() {
+
+        anim.SetTrigger("Reload");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = totalAmmo >= maxCountOfCurrentAmmo ? maxCountOfCurrentAmmo : totalAmmo;
+        totalAmmo -= currentAmmo;
+        availableSlots -= 1;
+        SetUI();
+
+    }
+
+    public void Shoot() {
+        
+        if (currentAmmo > 0) {
+
+            Flare.Play();
+            currentAmmo--;
+
+            RaycastHit hit;
+            if (Physics.Raycast(camTransform.transform.position, camTransform.transform.forward, out hit, range)) {
+                /*
+                 * Do Damage...
+                 * Enemy enemy = hit.transform.GetComponent<Enemy>();
+                 * enemy.takeDamage(weaponDamage);
+                 */
+
+                //Chain Destroying...
+                GameObject hitedObject = hit.transform.gameObject;
+                if (hitedObject.tag.Equals("Chain"))
+                    Destroy(hitedObject);
+
+            }
+
+            GameObject generatedExplosion = Instantiate(explosion, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(generatedExplosion, 0.2f);
+            SetUI();
+        }
+
+    }
+
+    public void AddBullets(int bulletPickUpCount) {
+        totalAmmo += bulletPickUpCount;
+        availableSlots = (int)Mathf.Ceil(totalAmmo / maxCountOfCurrentAmmo);
+        SetUI();
+    }
+
+    public void SetUI() {
+        currentAmmoCountText.text = currentAmmo.ToString();
+        slotsText.text = availableSlots.ToString();
+    }
+
+}
